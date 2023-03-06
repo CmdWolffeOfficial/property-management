a63ff218bbb89c0d1f3842b6f2eb92b69d12d2c9
CmdWolffeOfficial <admin@wolffe.dev>
2023-03-06 14:49:37 +0100

    ApplicationStatus Enum

--- src/main/java/dev/wolffe/property_management_example/domain/application/ApplicationStatus.java
+++ src/main/java/dev/wolffe/property_management_example/domain/application/ApplicationStatus.java
@@ -1,4 +1,15 @@
 package dev.wolffe.property_management_example.domain.application;
 
+
 public enum ApplicationStatus {
+    ACCEPTED("accepted"), DENIED("denied"), PENDING("pending");
+    private final String applicationStatus;
+
+    ApplicationStatus(String applicationStatus) {
+        this.applicationStatus = applicationStatus;
+    }
+
+    public String getApplicationStatus() {
+        return applicationStatus;
+    }
 }
