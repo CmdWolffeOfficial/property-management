e9c0472d4997e177488f4db87c888c3c412b689a
CmdWolffeOfficial <admin@wolffe.dev>
2023-03-06 14:17:25 +0100

    PropertyRepository

--- src/main/java/dev/wolffe/property_management_example/domain/property/PropertyRepository.java
+++ src/main/java/dev/wolffe/property_management_example/domain/property/PropertyRepository.java
@@ -1,4 +1,16 @@
 package dev.wolffe.property_management_example.domain.property;
 
-public interface PropertyRepository {
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+public interface PropertyRepository extends JpaRepository<Property, UUID> {
+    List<Property> findAllByPropertyCanton(String propertyCanton);
+
+    Property findByPropertyNameLike(String propertyName);
 }
+
