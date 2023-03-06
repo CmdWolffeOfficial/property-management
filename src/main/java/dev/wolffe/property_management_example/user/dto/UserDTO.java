7eaffb6933f6b7158030f64103caff5e334ca726
CmdWolffeOfficial <admin@wolffe.dev>
2023-03-06 13:19:24 +0100

    User DTO and Mapper

--- src/main/java/dev/wolffe/property_management_example/user/dto/UserDTO.java
+++ src/main/java/dev/wolffe/property_management_example/user/dto/UserDTO.java
@@ -1,4 +1,31 @@
 package dev.wolffe.property_management_example.user.dto;
 
+
 public class UserDTO {
+
+
+    private String userEmail;
+    private String userName;
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
 }
+
+
+
+
+
+
--- src/main/java/dev/wolffe/property_management_example/user/dto/UserMapper.java
+++ src/main/java/dev/wolffe/property_management_example/user/dto/UserMapper.java
@@ -1,4 +1,15 @@
 package dev.wolffe.property_management_example.user.dto;
 
+
+import dev.wolffe.property_management_example.user.User;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
 public interface UserMapper {
+    UserDTO toDTO(User user);
+
+    User fromDTO(UserDTO dto);
+
+
 }
+
