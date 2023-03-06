61732ce11784ae7b6ffc8bcbecc9f58f48dedea1
CmdWolffeOfficial <admin@wolffe.dev>
2023-03-06 13:46:14 +0100

    RoleController and RoleService

--- src/main/java/dev/wolffe/property_management_example/role/RoleController.java
+++ src/main/java/dev/wolffe/property_management_example/role/RoleController.java
@@ -1,4 +1,26 @@
 package dev.wolffe.property_management_example.role;
 
+
+import dev.wolffe.property_management_example.role.dto.RoleDTO;
+import dev.wolffe.property_management_example.role.dto.RoleMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/roles")
 public class RoleController {
+
+    @Autowired
+    private RoleService roleService;
+    @Autowired
+    private RoleMapper roleMapper;
+
+
+    @PostMapping("")
+    public ResponseEntity<RoleDTO> addRole(@RequestBody RoleDTO roleDTO) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(roleMapper.toDTO(roleService.addRole(roleMapper.fromDTO(roleDTO))));
+    }
 }
+
--- src/main/java/dev/wolffe/property_management_example/role/RoleService.java
+++ src/main/java/dev/wolffe/property_management_example/role/RoleService.java
@@ -1,4 +1,11 @@
 package dev.wolffe.property_management_example.role;
 
+
 public interface RoleService {
+
+    Role addRole(Role role);
+
+    Role findByName(String roleName);
+
 }
+
