4bbf177d3f9970f354a3e79b1df8c8e7d63d5a91
CmdWolffeOfficial <admin@wolffe.dev>
2023-03-06 09:36:55 +0100

    Initial Commit

---
+++ src/main/java/dev/wolffe/property_management_example/PropertyManagementExampleApplication.java
@@ -0,0 +1,13 @@
+package dev.wolffe.property_management_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropertyManagementExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PropertyManagementExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/wolffe/property_management_example/PropertyManagementExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.wolffe.property_management_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropertyManagementExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
