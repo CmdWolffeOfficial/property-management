43e8a557a96d9cdda3c978312e6cf5c30689b370
CmdWolffeOfficial <admin@wolffe.dev>
2023-03-06 14:24:45 +0100

    Application post and patch mapping

--- src/main/java/dev/wolffe/property_management_example/domain/application/ApplicationController.java
+++ src/main/java/dev/wolffe/property_management_example/domain/application/ApplicationController.java
@@ -1,4 +1,39 @@
 package dev.wolffe.property_management_example.domain.application;
 
+
+import dev.wolffe.property_management_example.domain.application.dto.ApplicationDTO;
+import dev.wolffe.property_management_example.domain.application.dto.ApplicationMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/applications")
 public class ApplicationController {
+
+    @Autowired
+    private ApplicationService applicationService;
+    @Autowired
+    private ApplicationMapper applicationMapper;
+
+
+    @PostMapping("/apply/{propertyId}/user/{userId}")
+    public ResponseEntity<ApplicationDTO> createApplication(@PathVariable("propertyId") UUID propertyId, @PathVariable("userId") UUID userId) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(applicationMapper.toDTO(applicationService.createApplication(userId, propertyId)));
+    }
+
+
+    @PatchMapping("/accept/{applicationId}/user/{userId}")
+    public ResponseEntity<ApplicationDTO> acceptApplication(@PathVariable("applicationId") UUID applicationId, @PathVariable("userId") UUID userId) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(applicationMapper.toDTO(applicationService.acceptApplication(userId, applicationId)));
+    }
+
+
+    @PatchMapping("/deny/{applicationId}/user/{userId}")
+    public ResponseEntity<ApplicationDTO> denyApplication(@PathVariable("applicationId") UUID applicationId, @PathVariable("userId") UUID userId) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(applicationMapper.toDTO(applicationService.denyApplication(userId, applicationId)));
+    }
 }
--- src/main/java/dev/wolffe/property_management_example/domain/application/ApplicationService.java
+++ src/main/java/dev/wolffe/property_management_example/domain/application/ApplicationService.java
@@ -1,4 +1,20 @@
 package dev.wolffe.property_management_example.domain.application;
 
+
+import java.util.UUID;
+
 public interface ApplicationService {
+
+    Application acceptApplication(UUID userId, UUID applicationId);
+
+
+    Application createApplication(UUID userId, UUID propertyId);
+
+    Application denyApplication(UUID userId, UUID applicationId);
+
+
+    Application findById(UUID id);
+
+
 }
+
