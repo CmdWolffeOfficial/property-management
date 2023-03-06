08db3e1a9940ffe60dc0174677203b9854f1d4af
CmdWolffeOfficial <admin@wolffe.dev>
2023-03-06 13:47:10 +0100

    RoleRepository

--- src/main/java/dev/wolffe/property_management_example/role/RoleRepository.java
+++ src/main/java/dev/wolffe/property_management_example/role/RoleRepository.java
@@ -1,4 +1,14 @@
 package dev.wolffe.property_management_example.role;
 
-public interface RoleRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, UUID> {
+
+    Optional<Role> findAllByRoleName(String roleName);
+
 }
+
