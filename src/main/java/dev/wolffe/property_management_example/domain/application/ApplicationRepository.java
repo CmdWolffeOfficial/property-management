3ad5f67eb4786889da5d391f4e10feda8079f271
CmdWolffeOfficial <admin@wolffe.dev>
2023-03-06 14:25:50 +0100

    ApplicationRepository

--- src/main/java/dev/wolffe/property_management_example/domain/application/ApplicationRepository.java
+++ src/main/java/dev/wolffe/property_management_example/domain/application/ApplicationRepository.java
@@ -1,4 +1,14 @@
 package dev.wolffe.property_management_example.domain.application;
 
-public interface ApplicationRepository {
+
+import dev.wolffe.property_management_example.domain.property.Property;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+public interface ApplicationRepository extends JpaRepository<Application, UUID> {
+    List<Application> findAllByApplicationProperty(Property property);
 }
