7f3ee821fd3dbd2a5017080f0c5a234348b2eebf
CmdWolffeOfficial <admin@wolffe.dev>
2023-03-06 13:31:11 +0100

    UserRepository

--- src/main/java/dev/wolffe/property_management_example/user/UserRepository.java
+++ src/main/java/dev/wolffe/property_management_example/user/UserRepository.java
@@ -1,4 +1,11 @@
 package dev.wolffe.property_management_example.user;
 
-public interface UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+    User findByUserEmailLike(String userEmail);
+    User findByUserNameLike(String userName);
 }
