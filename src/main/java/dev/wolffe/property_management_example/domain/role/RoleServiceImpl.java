54f89228fc3db88394cdd313722f0e79a9b84a33
CmdWolffeOfficial <admin@wolffe.dev>
2023-03-06 14:31:42 +0100

    RoleServiceImpl

--- src/main/java/dev/wolffe/property_management_example/domain/role/RoleServiceImpl.java
+++ src/main/java/dev/wolffe/property_management_example/domain/role/RoleServiceImpl.java
@@ -1,4 +1,29 @@
 package dev.wolffe.property_management_example.domain.role;
 
-public class RoleServiceImpl {
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
+@Service
+public class RoleServiceImpl implements RoleService {
+
+    @Autowired
+    private RoleRepository roleRepository;
+
+
+    @Override
+    public Role addRole(Role role) {
+        return roleRepository.save(role);
+    }
+
+    @Override
+    public Role findByName(String roleName) {
+        Optional<Role> optionalRole = roleRepository.findAllByRoleName(roleName);
+        if (optionalRole.isPresent()) {
+            return optionalRole.get();
+        }
+        throw new NoSuchElementException("No role found with name " + roleName);
+    }
 }
